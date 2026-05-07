1f72be93fe71422fcd8755eb1b1e4d741c8f1532
erwan <erwan@boreal-business.net>
2026-05-07 10:57:20 +0200

    update registration type code list

--- src/Webservice/Soap/Type/Common/Ship/RegistrationNumber.php
+++ src/Webservice/Soap/Type/Common/Ship/RegistrationNumber.php
@@ -13,38 +13,86 @@ class RegistrationNumber
     /** @var */
     var $NumberIssuerCountryCode;
 
-    /** Value-Added tax */
-    const VAT = 'VAT';
+    /** CNPJ/CPF Federal Tax */
+    const CNP = 'CNP';
+
+    /** Deferment Account Duties Only */
+    const DAN = 'DAN';
+
+    /** Deferment Account Duties, Taxes and Fees Only */
+    const DTF = 'DTF';
+
+    /** Data Universal Numbering System */
+    const DUN = 'DUN';
+
     /** Employer Identification Number */
     const EIN = 'EIN';
-    /** Goods and Service Tax */
-    const GST = 'GST';
-    /** Social Security Number */
-    const SSN = 'SSN';
-    /** European Union Registration and Identification */
-    const EORI = 'EOR';
-    /** Data Universal Number System */
-    const DUNS = 'DUN';
+
+    /** Economic Operator Registration ID */
+    const EOR = 'EOR';
+
     /** Federal Tax ID */
     const FED = 'FED';
+
+    /** Free Trade Zone ID */
+    const FTZ = 'FTZ';
+
+    /** Manufacturers Identification Code */
+    const MID = 'MID';
+
+    /** National Identity Card */
+    const NID = 'NID';
+
+    /** Passport */
+    const PAS = 'PAS';
+
+    /** EU Registered Exporters Registration ID */
+    const RGP = 'RGP';
+
+    /** Import One-Stop-Shop */
+    const SDT_IOSS = 'SDT';
+
+    /** Overseas Registered Supplier */
+    const SDT_OVERSEAS_REGISTERED_SUPPLIER = 'SDT';
+
+    /** AUSid GST Registration */
+    const SDT_AUSID_GST_REGISTRATION = 'SDT';
+
+    /** GB VAT Foreign Registration */
+    const SDT_GB_VAT_FOREIGN_REGISTRATION = 'SDT';
+
+    /** VAT on E-Commerce */
+    const SDT_VAT_ON_ECOMMERCE = 'SDT';
+
+    /** OVR GSTN */
+    const SDT_OVR_GSTN = 'SDT';
+
+    /** LVG Registration Number */
+    const SDT_LVG_REGISTRATION_NUMBER = 'SDT';
+
+    /** Social Security Number */
+    const SSN = 'SSN';
+
     /** State Tax ID */
-    const STA = 'STA';
-    /** Brazil CNPJ/CPF Federal Tax */
-    const CNPJ = 'CNP';
-    /** Brazil type IE/RG Federal Tax */
-    const IE = 'IE';
-    /** Russia bank details section - INN */
-    const INN = 'INN';
-    /** Russia bank details section - KPP */
-    const KPP = 'KPP';
-    /** Russia bank details section - OGRN */
-    const OGRN = 'OGR';
-    /** Russia bank details section – OKPO */
-    const OKPO = 'OKP';
-    /** Germany Movement Reference Number */
-    const MRN = 'MRN';
-    /** Overseas Registered Supplier GST Number */
-    const OSR = 'OSR';
+    const STA_STATE_TAX_ID = 'STA';
+
+    /** Brazil State Tax ID IE/RG */
+    const STA_BRAZIL_STATE_TAX_ID = 'STA';
+
+    /** Subsidiary Number */
+    const SUB = 'SUB';
+
+    /** Deferment Account Tax Only */
+    const TAN = 'TAN';
+
+    /** VAT Registration */
+    const VAT = 'VAT';
+
+    /** Internal Market Scheme */
+    const IMS = 'IMS';
+
+    /** eInvoice Carrier */
+    const EIC = 'EIC';
 
     public function __construct($number, $numberTypeCode, $countryCode)
     {
